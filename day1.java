8f58ad4f69036d7290efeed976986e07eb56ad6c
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-05-30 11:37:03 +0530

    Day1

---
+++ day1.java
@@ -0,0 +1,40 @@
+// git remote add origin https://github.com/tarun-954/DSA-in-Java.git
+// git branch -M main
+// git push -u origin main
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+public class day1{
+    public static void main(String[] args){
+    System.out.println("Hello world! DSA in Java");
+    System.out.println("Start today or never");
+
+ }
+ }
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*  design a pattern in java
+
+    *
+    **
+    ***
+    *****
+    ****** 
+
+    */
+import java.util.*;
+    public class day1{
+public static void main(String[] agrs)
+
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+
+}    
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    
