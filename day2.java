cd2de0cb51d213b4e3cef744c280fa730ce36ed9
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-01 22:20:00 +0530

    Day2 night commit

--- day2.java
+++ day2.java
@@ -157,3 +157,71 @@ switch(c){
  */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Ask the user to enter the number of the month & print the name of the month. For eg - For ‘1’ print ‘January’, ‘2’ print ‘February’ & so on.
+
+/*
+
+import java.util.*;
+ 
+public class day2 {
+public static void main(String[] args){
+    System.out.println("Enter a number");
+    Scanner sc=new Scanner(System.in);
+   
+    int value=sc.nextInt();
+    // function called 
+    checkmonth(value);
+}
+
+
+// function made 
+static void checkmonth(int value){
+switch (value) {
+    case 1:
+      System.out.println("January");
+        break;
+        case 2:
+        System.out.println("Febuary");
+          break;
+          case 3:
+          System.out.println("March");
+            break;
+            case 4:
+            System.out.println("April");
+              break;
+              case 5:
+              System.out.println("May");
+                break;
+                case 6:
+                System.out.println("June");
+                  break;
+                  case 7:
+                  System.out.println("July");
+                    break;
+                    case 8:
+                    System.out.println("August");
+                      break;
+                      case 9:
+                      System.out.println("September");
+                        break;
+                        case 10:
+                        System.out.println("Octuber");
+                          break;
+                          case 11:
+                          System.out.println("November");
+                            break;
+                            case 12:
+                            System.out.println("December");
+                              break;
+
+
+    
+    default:
+    System.out.println("Please enter valid month");
+        break;
+}
+}
+    
+}
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
