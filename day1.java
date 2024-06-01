aa8dd3984548ffca82677816fca5b5fbc4170d2f
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-01 11:16:46 +0530

    day1 completed

--- day1.java
+++ day1.java
@@ -1,5 +1,7 @@
 // git remote add origin https://github.com/tarun-954/DSA-in-Java.git
 // git branch -M main
+// git commit -m "Day1"
+// git push -u origin main
 // git push -u origin main
 
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -23,6 +25,8 @@ public class day1{
     ****** 
 
     */
+/*
+
 import java.util.*;
     public class day1{
 public static void main(String[] agrs)
@@ -31,10 +35,45 @@ public static void main(String[] agrs)
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 
+for(int i=0;i<n;i++){
+    for(int j=0;j<=i;j++){
+System.out.print("*");
+
+    }
+    System.out.println(" ");
+}
 }    
     }
-
+*/
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// print priamid of stars 
+import java.util.*;
+
+public class day1{
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+        for(int i=0;i<=n;i++){
+         for(int j=0;j<=n-i-1;j++){
+            System.out.print(" ");
+         }
+                for(int j=0;j<i;j++){
+                    System.out.print(" ");
+                    System.out.print("*");
+                }
+                System.out.println();
+        }
+     sc.close();
+    }
+}
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
     
