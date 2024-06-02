e5d1871ec750bd3f0cc7065d1b0ca3f268ddeb40
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-02 16:20:00 +0530

    Afternoon commit 2

--- day3.java
+++ day3.java
@@ -135,6 +135,8 @@ public class day3{
  *  *  *  *  *
  
  */
+
+ /*
 import java.util.*;
 public class day3{
     public static void main(String[] args){
@@ -149,20 +151,77 @@ public class day3{
             else{
             System.out.print("   ");
             }
-
         }
             System.out.println();
     }
 }
 }
 
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//print the pattern
+// half piramid 
+/*
+
+ * 
+ * *
+ * * *
+ * * * *
+ * * * * *
+ 
+ */
+/* 
+import java.util.*;
+public class day3{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<i+1;j++){
+                System.out.print("* ");
+            }
+
+
+            System.out.println();
+        }
+    }
+}
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+//print the pattern
+// half piramid 
+
+/*
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+import java.util.*;
+public class day3{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+
+            for(int j=0;j<n-i;j++){
+                System.out.print("* ");
+            }
 
 
-
-
+            System.out.println();
+        }
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
