d9d7cbf8eebc27e1c019a7916fb411a94f10781b
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-04 07:58:01 +0530

    Morning commit

---
+++ day5.java
@@ -0,0 +1,33 @@
+
+// Print the pattern 
+
+                       /*     palindromic  Piramid      */
+
+/*
+                        1
+                      2 1 2
+                    3 2 1 2 3
+                  4 3 2 1 2 3 4
+                5 4 3 2 1 2 3 4 5
+                 
+                 */
+
+import java.util.*;
+public class day5 {
+    public static void main(String[] agrs){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+         for(int i=1;i<=n;i++){
+            for(int k=i;k<=n;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                    System.out.print(j+" ");
+
+            }
+            System.out.println();
+         }
+         sc.close();                                                                                       
+    }
+}
