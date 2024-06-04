31bdcab78178275208b81a669e30d598e46bf4ac
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-04 14:33:05 +0530

    After noon commit

--- day5.java
+++ day5.java
@@ -17,17 +17,25 @@ public class day5 {
     public static void main(String[] agrs){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        
-         for(int i=1;i<=n;i++){
-            for(int k=i;k<=n;k++){
-                System.out.print(" ");
+ 
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print("  ");
             }
-            for(int j=1;j<=i;j++){
-                    System.out.print(j+" ");
 
+            for(int j=i;j>=1;j--){
+                System.out.print(j+ " ");
+            
             }
+            for(int j=2;j<=i;j++){
+
+            System.out.print(j+" ");
+            }
+        
             System.out.println();
-         }
-         sc.close();                                                                                       
+        }
+         sc.close();
+
     }
+
 }
