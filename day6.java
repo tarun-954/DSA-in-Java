beda4c3cb121271bf7f19da0b88087ea51c36bcb
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-05 15:43:58 +0530

    Afternoon commit

---
+++ day6.java
@@ -0,0 +1,28 @@
+/* Functions and methods praactice questions  */
+//Q1
+
+// Enter 3 numbers  from the user and make a function to print their average
+/*
+import java.util.*;
+public class day6 {
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int result=average(a,b,c);
+System.out.println(result);
+
+}
+public static int average(int a,int b,int c){
+return (a+b+c)/3;
+
+}
+}
+    */ 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// Q2
+// write a function to print the sum of all odd numbers from 1 to n ;
+
+
