7507839dae28bd8aaf8c3f5d1f6db6719858c6b6
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-05 17:48:20 +0530

    Evening commit

--- day6.java
+++ day6.java
@@ -24,5 +24,73 @@ return (a+b+c)/3;
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // Q2
 // write a function to print the sum of all odd numbers from 1 to n ;
+  /*
 
+import java.util.*;
+public class day6{
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int n= sc.nextInt();
+    int result=sumOfOddNumbers(n);
+System.out.println(result);
 
+    }
+    public static int sumOfOddNumbers(int n){
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                sum=sum+i;
+            }
+                   }
+       return sum;
+                }
+            }
+
+           */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// write a functin to take two input from the user and return the greater of those 
+/*
+import java.util.*;
+public class day6{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        long a=sc.nextLong();
+        long b=sc.nextLong();
+
+        long result=greater(a,b);
+        System.out.println(result);
+    }
+    public static long greater(long a,long b){
+        if(a==b){
+            return 0;
+        }
+        if(a>b){
+            return a;
+        }
+        else{
+            return b;
+        }
+    }
+}
+
+ */
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ // write a function that takes in the radius as input and return the circumfrence of a circle 
+
+ /*
+ import java.util.*;
+ public class day6{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        double result = circumfrence(r);
+        System.out.printf("%.2f",result);
+    }
+public static double circumfrence(int r){
+    return 2*Math.PI*r;
+}
+ }
+ */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
