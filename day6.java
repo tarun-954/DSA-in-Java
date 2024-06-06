ab10c0b27006d5993f752c535617b6b19c924922
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-06 10:23:37 +0530

    Morning commit

--- day6.java
+++ day6.java
@@ -239,11 +239,4 @@ sc.close();
 }
  */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*         Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e. x power n.              */
 
-import java.util.*;
- public class day6{
-    public static void main(String[] args){
-        
-    }
- }
---
+++ day7.java
@@ -0,0 +1,27 @@
+
+/*       
+Two numbers are entered by the user, x and n. Write a function to find the value of 
+one number raised to the power of another i.e. x power n.              */
+
+/*
+
+import java.util.*;
+ public class day7{
+    public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         int x=sc.nextInt();
+        double result=power(n,x);
+        System.out.printf("%.0f",result);
+    }
+    public static double power(double n,double x){
+       return Math.pow(n,x);
+    }
+ }
+  */
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
