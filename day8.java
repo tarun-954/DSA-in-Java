33ebfc4ff5657c8989a7083e2d5aa69143e700f8
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-07 12:01:28 +0530

    After noon commit

--- day8.java
+++ day8.java
@@ -1,4 +1,5 @@
                         /* 2D array in java */
+  /*
 import java.util.*;
 
 public class day8{
@@ -22,8 +23,43 @@ public class day8{
         }
     }
 }
+ */
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/* Take a matrix from the user. Search for a given number x and print indices at which it occurs  */
+
+import java.util.*;
+public class day8{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int row=sc.nextInt();
+        int coln=sc.nextInt();
+        int[][] array=new int[row][coln];
+        // creating 2D matrix
+        for(int i=0;i<row;i++){
+            for(int j=0;j<coln;j++){
+           array[i][j]=sc.nextInt(); 
+
+            }
+        }
+            for(int i=0;i<row;i++){
+                for(int j=0;j<coln;j++){
+                    System.out.print(array[i][j]+" ");
+                }
+                System.out.println();
+
+            }
+            int x=sc.nextInt();
+            for(int i=0;i<row;i++){
+                for(int j=0;j<coln;j++){
+                    if(array[i][j]==  x ){
+                        System.out.println(i+" " +j);
+                    }
+                }
+            }
+
+    }
+}
 
 
 
