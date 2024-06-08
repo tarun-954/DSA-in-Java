e11149b91d6e8c022fb26eea63b5fac9ac0814bd
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-08 18:29:40 +0530

    Evening coommit 2

--- day9.java
+++ day9.java
@@ -1,3 +1,5 @@
+/*
+
 import java.util.*;
 public class day9{
     public static void main(String[] args){
@@ -6,6 +8,24 @@ public class day9{
         String firstname=sc.nextLine();
         String lastname=sc.nextLine();
         System.out.println(firstname+" "+lastname);
-        
+
+    }
+}
+
+ */
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+public class day9{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        String firstname=sc.nextLine();
+        String lastname=sc.nextLine();
+        System.out.println(firstname+" "+lastname);
+        // finding the length 
+        // use inbuild length function
+        String fullName=firstname+lastname;
+        System.out.println(fullName.length());
     }
 }
