8cd9614f5c9c5a1d3c910bc60d684cc7c6af2d46
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-08 19:09:56 +0530

    Evening commit 3

--- day9.java
+++ day9.java
@@ -14,7 +14,7 @@ public class day9{
 
  */
 /////////////////////////////////////////////////////////////////////////////////////////////
-
+/* 
 import java.util.*;
 public class day9{
     public static void main(String[] args){
@@ -27,5 +27,35 @@ public class day9{
         // use inbuild length function
         String fullName=firstname+lastname;
         System.out.println(fullName.length());
-    }
+
+
+        // Access all the elements in String
+        for(int i=0;i<fullName.length();i++)
+{
+System.out.println(fullName.charAt(i));
+}    }
+}
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// comparing the strings in java 
+
+import java.util.*;
+public class day9{
+    public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String name1=sc.nextLine();
+String name2=sc.nextLine();
+
+if(name1.compareTo(name2)==0){
+System.out.println("String are equals");
+
+
+}
+else{
+    System.out.println("Strings are not equal");
+}
+}
+
 }
