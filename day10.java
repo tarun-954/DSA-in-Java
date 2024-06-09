dcbc313022eda9a9eb9a2934e4df748979955df6
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-06-09 22:59:28 +0530

    night commit 2

--- day10.java
+++ day10.java
@@ -25,5 +25,15 @@ System.out.println(s6);
 String name="javatpoint";  
 char ch1=name.charAt(4);//returns the char value at the 4th index  
 System.out.println(ch1);  
+
+// java String format()
+String name2="sonoo";  
+String sf1=String.format("name is %s",name2);  
+String sf2=String.format("value is %f",32.33434);  
+String sf3=String.format("value is %32.12f",32.33434);//returns 12 char fractional part filling with 0  
+  
+System.out.println(sf1);  
+System.out.println(sf2);  
+System.out.println(sf3);  
     }
 }
