f6ef9c1009d366c201150849cd4515caa81154c2
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-07-06 19:31:58 +0530

    One shot java practice

--- One_Shot_practice.java
+++ One_Shot_practice.java
@@ -20,15 +20,37 @@ public class One_Shot_practice
     {
             // basic program print statement
         //System.out.print("hello world");
-        
+        /*
+        byte  - 1 
+        short - 2 
+        int  -4 
+        long - 8
+        float - 4
+        double - 8
+        char - 2
+        boolean - 1
+ */
+
         // variables in java
         String name="Tarun choudhary";
-        int age =43455362;
+        int age =1234567890;
 
         System.out.println(name);
         System.out.println(age);
         // length of the string
         System.out.println(name.length());
 
+        //Strings in details
+        // concatenate 
+        String name1="Tarun";
+        String lastName=" Choudhary";
+        System.out.println(name1+lastName);
+        //to print that char at the index
+        //char at 
+        System.out.println(name1.charAt(0));
+
+        // replace
+        String name2=name1.replace('T','t');
+        System.out.println(name2);
     }
 }
