dbad0984d10d696f466736a8f033bf74c5fe8b1a
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-07-06 19:03:20 +0530

    One shot Java

---
+++ One_Shot_practice.java
@@ -0,0 +1,34 @@
+// import java.util.*;
+
+// public class One_shot_java_practice 
+// {
+
+//     public static void main(String[] args){
+   
+//         System.out.println("Hello World");
+//         System.out.print("Tanmay");
+
+       
+//     }
+
+// }
+
+public class One_Shot_practice
+{
+
+    public static void main (String[] args)
+    {
+            // basic program print statement
+        //System.out.print("hello world");
+        
+        // variables in java
+        String name="Tarun choudhary";
+        int age =43455362;
+
+        System.out.println(name);
+        System.out.println(age);
+        // length of the string
+        System.out.println(name.length());
+
+    }
+}
